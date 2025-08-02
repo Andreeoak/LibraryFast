30e6c15aa5fc859cb93194847d20a7f2b1baeefd
AndreeOak <andreesoul@gmail.com>
2025-08-02 15:40:27 -0300

    Defining mockdata

--- books.py
+++ books.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
+from mockData import Library
 
 # python -m uvicorn books:app --reload
 
 app = FastAPI()
+booksAvailable = Library.getInventory()
 
 @app.get("/")
 async def first_call():
-    return {
-        "Dedeius":"Hello!"
-    }
+    return booksAvailable
