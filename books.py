36bd1da5a23b67bfc8088322fdca0e1941a532ec
AndreeOak <andreesoul@gmail.com>
2025-08-03 10:40:43 -0300

    More get requests (dynamic parameters and query parameters)

--- books.py
+++ books.py
@@ -1,17 +1,30 @@
 from fastapi import FastAPI
 from mockData import Library
 
-# python -m uvicorn books:app --reload
+# python -m uvicorn books:app --reload  //development: dont use --reload on prod and aways specify the --host 
 
 app = FastAPI()
-booksAvailable = Library.getInventory()
+books_available = Library.getInventory()
+
 
 @app.get("/books")
 async def getAllBooks():
-    return booksAvailable
+    return books_available
 
 @app.get("/books/{title}")
-async def getBookByTitle(title):
-    return [book for book in booksAvailable if (book.get("title").casefold() == title.casefold())]
+async def getBookByTitle(title: str): 
+    return [book for book in books_available if (book.get("title").casefold() == title.casefold())]
         
-         
+
+@app.get("/books/")
+async def getBooksByCategory(category: str):
+    return [book for book in books_available if(book.get("category").casefold() == category.casefold())]
+
+
+@app.get("/books/{book_author}/")
+async def getBooksByCategoryFromAuthor(book_author:str, category:str):
+    return [book for book in books_available 
+            if(book.get("category").casefold() == category.casefold()
+               and
+               book.get("author").casefold() == book_author.casefold())
+            ]
